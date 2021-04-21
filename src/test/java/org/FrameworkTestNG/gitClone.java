5020c51829a225c361d73047b0a6c7e61cf7f503
rajaramanangithub <rajaramananacecse@gmail.com>
2021-04-22 00:06:13 +0530

    updated gitclone java class user1

---
+++ src/test/java/org/FrameworkTestNG/gitClone.java
@@ -0,0 +1,12 @@
+package org.FrameworkTestNG;
+
+public class gitClone {
+
+	public static void main(String[] args) {
+		System.out.println("Test");
+		
+		System.out.println("Test from user1");
+
+	}
+
+}
